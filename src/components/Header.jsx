839b636971e9c7274e8efabe414ee56088d16232
mivchal <mivchal@hotmail.com>
2020-09-11 20:00:38 +0200

    always visible header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import {Link} from 'react-router-dom';
 
 const Header = () => {
     return (
         <div className="ui secondary pointing menu">
+            <Link to="/" className="item">
+                Streamy
+            </Link>
             <div className="right menu">
-                <Link to='/' className="item">
-                    Streamy
+                <Link to="/" className="item">
+                    All Streams
                 </Link>
-                <div className="right menu">
-                    <Link to="/" className="item">
-                        All Streams
-                    </Link>
-                </div>
             </div>
         </div>
     );
