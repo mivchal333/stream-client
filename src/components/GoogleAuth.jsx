503aee5a5921aa25d93dbf1d13c27cedfa803ff7
mivchal <mivchal@hotmail.com>
2020-09-11 22:12:59 +0200

    user id after sign in

--- src/components/GoogleAuth.jsx
+++ src/components/GoogleAuth.jsx
@@ -29,7 +29,7 @@ class GoogleAuth extends React.Component {
     };
 
     onSignInClick = () => {
-        this.auth.signIn();
+        this.auth.signIn(this.auth.currentUser.get().getId());
     };
 
     onSignOutClick = () => {
