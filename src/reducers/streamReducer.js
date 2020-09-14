58e91f105c32b546169d44781a6fd8610d49d22e
mivchal <mivchal@hotmail.com>
2020-09-14 22:27:33 +0200

    stream reducer

---
+++ src/reducers/streamReducer.js
@@ -0,0 +1,10 @@
+import {CREATE_STREAM, EDIT_STREAM, FETCH_STREAM} from "../actions/types";
+
+export default (state = {}, action) => {
+    switch (action.type) {
+        case FETCH_STREAM || CREATE_STREAM || EDIT_STREAM:
+            return {...state, [action.payload.id]: action.payload}
+        default:
+            return state;
+    }
+}
