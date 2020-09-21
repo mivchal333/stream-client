4c0df857d8880c1a4c98e5ad7b0278758e4ecbda
mivchal <mivchal@hotmail.com>
2020-09-21 21:51:05 +0200

    streams reducer, fetch streams reducer

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -1,9 +1,14 @@
-import {CREATE_STREAM, EDIT_STREAM, FETCH_STREAM} from "../actions/types";
+import {CREATE_STREAM, DELETE_STREAM, EDIT_STREAM, FETCH_STREAM, FETCH_STREAMS} from "../actions/types";
+import _ from 'lodash'
 
 export default (state = {}, action) => {
     switch (action.type) {
+        case FETCH_STREAMS:
+            return {...state, ..._.mapKeys(action.payload, 'id')}
         case FETCH_STREAM || CREATE_STREAM || EDIT_STREAM:
             return {...state, [action.payload.id]: action.payload}
+        case DELETE_STREAM:
+            return _.omit(state, action.payload)
         default:
             return state;
     }
